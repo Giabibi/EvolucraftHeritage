cdb941ea257920474d1ae6ddcf348692aabef200
Leny Wilhelm <leny.wilhelm@gmail.com>
2025-07-16 11:32:19 +0200

    Only authorize Heritage commands for specifics servers

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,7 +1,9 @@
-import Discord from "discord.js";
+import Discord, { MessageFlags } from "discord.js";
 import path from "path";
 import { ClientWithCommands, Command } from "../types/discord";
 import { convertDateToString } from "../utils/convert";
+
+const authorizedGuildsIds: string[] = ["707715843018195035"];
 
 export default async (
     bot: ClientWithCommands,
@@ -20,11 +22,29 @@ export default async (
         const commandModule = await import(
             path.join(commandsPath, interaction.commandName)
         );
-        const command: Command = commandModule.default; 
+        const command: Command = commandModule.default;
+        if (
+            command.category === "Héritage" &&
+            interaction.context === Discord.InteractionContextType.Guild &&
+            interaction.guildId &&
+            !authorizedGuildsIds.includes(interaction.guildId)
+        ) {
+            await interaction.reply({
+                content: `⛔ Vous ne pouvez pas interagir avec des commandes de cette catégorie sans être dans le serveur adéquate.`,
+                flags: MessageFlags.Ephemeral,
+            });
+            return;
+        }
         try {
             command.run(bot, interaction, command.options);
         } catch (e) {
-            console.error(`[${command.name}][${convertDateToString(new Date(Date.now()), "yyyy-MM-dd hh:mm")}] Error:\n`, e)
+            console.error(
+                `[${command.name}][${convertDateToString(
+                    new Date(Date.now()),
+                    "yyyy-MM-dd hh:mm"
+                )}] Error:\n`,
+                e
+            );
         }
     }
     if (
